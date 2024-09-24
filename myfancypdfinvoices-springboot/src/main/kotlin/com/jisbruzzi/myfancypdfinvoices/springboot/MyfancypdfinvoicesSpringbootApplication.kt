9bc84871643e840baef28a9f1cd13f10a0681157
jisbruzzi <jose.sbru@gmail.com>
2024-09-24 15:12:45 -0300

    add spring boot version

---
+++ myfancypdfinvoices-springboot/src/main/kotlin/com/jisbruzzi/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.jisbruzzi.myfancypdfinvoices.springboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyfancypdfinvoicesSpringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyfancypdfinvoicesSpringbootApplication>(*args)
+}
