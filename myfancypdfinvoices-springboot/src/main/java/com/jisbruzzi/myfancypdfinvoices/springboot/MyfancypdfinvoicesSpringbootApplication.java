2bb8d060ad2be02d8f46d57908e4d005174148b2
jisbruzzi <jose.sbru@gmail.com>
2024-09-25 10:13:03 -0300

    mod 7

--- myfancypdfinvoices-springboot/src/main/java/com/jisbruzzi/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
+++ myfancypdfinvoices-springboot/src/main/java/com/jisbruzzi/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplication.java
@@ -1,13 +1,24 @@
 package com.jisbruzzi.myfancypdfinvoices.springboot;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MyfancypdfinvoicesSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfancypdfinvoicesSpringbootApplication.class, args);
+	}
+
+	@Bean
+	public InitializingBean runner(DataSource dataSource) {
+		return () -> {
+			System.out.println("This is the datasource this Spring Boot project is using: " + dataSource);
+		};
 	}
 
 }
